381488952da3f43da8547eb1f422caca137a4bd0
Shaleha Khanam <khanamshaleha@yahoo.com.au>
2024-04-19 22:34:14 +1000

    created layout, homepae and style

---
+++ app/layout.jsx
@@ -0,0 +1,23 @@
+import '@/assets/styles/globals.css';
+
+export const metadata = {
+    title: 'PropertyPulse | Find The Perfect Rental',
+    description: 'Find your dream rental property',
+    keywords: 'rental, find rentals, find properties, find dream rental property'
+
+};
+const MainLayout = ( {children}) => {
+  return (
+    <html lang="en">
+        <body>
+            <div>
+                {children}
+            </div>
+        </body>
+
+    </html>
+   
+  )
+}
+
+export default MainLayout
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+    title: 'Home Page'
+};
+const HomePage = () => {
+  return (
+    <div>
+      HomePage
+    </div>
+  )
+}
+
+export default HomePage
